12bc121b9c8e99590729f71ca1b51c106b8e1801
Juliana Melo <54086293+julianakcm1@users.noreply.github.com>
2020-05-21 11:26:45 -0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,5 +1,5 @@
-print("Metros: ")
-metros = int(input())
+#print("Metros: ")
+metros = int(input("Digite a quantidade de metros:\n"))
 
 centimetros = metros * 100
 print(centimetros, "centímetros")
