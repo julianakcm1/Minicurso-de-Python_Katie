ae9471792574904d1bb6410d1859fb3f661c37cf
Juliana Melo <54086293+julianakcm1@users.noreply.github.com>
2020-05-21 11:25:10 -0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -3,6 +3,7 @@ peso = float(input("Digite o peso:\n"))
 #print("Digite a altura:")
 altura = float(input("Digite a altura:\n"))
 
-imc = peso / (altura*altura)
+#imc = peso / (altura*altura)
+imc = peso / (altura**2)
 
 print("IMC = {:.4f}".format(imc))
