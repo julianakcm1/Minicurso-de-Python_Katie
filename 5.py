bed699ba790b8a68959bed1a42d96fa996823365
Juliana Melo <54086293+julianakcm1@users.noreply.github.com>
2020-05-21 11:38:52 -0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,16 +1,13 @@
-def function (espaco, tempo, velocidade):
+def function (espaco, tempo):
     velocidade_media = espaco/tempo
     aceleracao = velocidade_media/tempo
     print("Velocidade média = {:.5f}".format(velocidade_media))
     print("Aceleração = {:.5f}".format(aceleracao))
 
-print("Digite o espaço (em metros): ")
-espaco = float(input())
+#print("Digite o espaço (em metros): ")
+espaco = float(input("Digite o espaço (em metros): "))
 
-print("Digite o tempo (em segundos): ")
-tempo = float(input())
+#print("Digite o tempo (em segundos): ")
+tempo = float(input("Digite o tempo (em segundos): "))
 
-print("Digite a velocidade (em metros/segundo): ")
-velocidade = float(input())
-
-function(espaco, tempo, velocidade)
+function(espaco, tempo)
